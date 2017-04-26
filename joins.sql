85e3c11630933c7a11b53dac164ac8b7acde444c
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-04-26 11:46:11 -1000

    13 queries done

--- joins.sql
+++ joins.sql
@@ -41,7 +41,27 @@
 -- INNER JOIN posts p ON c.post_id = p.id
 -- WHERE p.created_at > '2015-01-01'::date;
 
-SELECT p.title as post_title, p.url as post_url, c.body as comment_body
+-- SELECT p.title as post_title, p.url as post_url, c.body as comment_body
+-- FROM comments c
+-- INNER JOIN posts p ON c.post_id = p.id
+-- WHERE c.body LIKE '%USB%'
+
+-- SELECT p.title as post_title, u.firstname, u.lastname, c.body as comment_body
+-- FROM comments c
+-- INNER JOIN posts p ON c.post_id = p.id
+-- INNER JOIN users u ON p.user_id = u.id
+-- WHERE c.body LIKE '%matrix%';
+
+-- SELECT u.firstname, u.lastname, c.body as comment_body
+-- FROM comments c
+-- INNER JOIN posts p ON c.post_id = p.id
+-- INNER JOIN users u ON p.user_id = u.id
+-- WHERE c.body LIKE '%SSL%'
+-- AND p.content LIKE '%dolorum%'
+
+SELECT u.firstname as post_author_first_name, u.lastname as post_author_last_name, p.title as post_title, u.username as comment_author_username, c.body as comment_body
 FROM comments c
 INNER JOIN posts p ON c.post_id = p.id
-WHERE c.body LIKE '%USB%'
+INNER JOIN users u ON p.user_id = u.id
+WHERE (c.body LIKE '%SSL%' OR c.body LIKE '%firewall%')
+AND p.content LIKE '%nemo%';
